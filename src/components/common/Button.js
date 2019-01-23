016fb04e7fa61c7d2b98e8bed539a056aac7c9a5
oliverpople <oliverpople@gmail.com>
2019-01-23 10:16:51 -0800

    Imported Header component and rendered in App.js.

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
@@ -13,20 +13,20 @@ const Button = ({ onPress, children }) => {
 
 const styles = {
   textStyle: {
-    alignSelf: "center",
-    color: "#007aff",
+    alignSelf: 'center',
+    color: '#007aff',
     fontSize: 16,
-    fontWeight: "600",
+    fontWeight: '600',
     paddingTop: 10,
     paddingBottom: 10
   },
   buttonStyle: {
     flex: 1,
-    alignSelf: "stretch",
-    backgroundColor: "#fff",
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: "#007aff",
+    borderColor: '#007aff',
     marginLeft: 5,
     marginRight: 5
   }
